8526494c682dd46a91c8260e4342d817f44598d1
Santeri Suomala <santeri.suomala@outlook.com>
2023-09-04 21:56:43 +0300

    did something

--- src/main.py
+++ src/main.py
@@ -1,51 +1,11 @@
 import random
 
-import mpv
-
 from flask import Flask, render_template, request
 
-
-class Player:
-    def __init__(self):
-        self._mpv = mpv.MPV()
-        self._stations = self._getStations()
-        self._currentStation = random.choice(self._stations)
-
-        self.playing = False
-
-    def getStation(self):
-        return self._currentStation
-
-    def setStation(self, station):
-        self._currentStation = station
-
-    def changeStation(self):
-        new = random.choice(self._stations)
-        if new == self._currentStation:
-            self.changeStation()
-        else:
-            self.setStation(new)
-            self.play()
-
-    def play(self):
-        self._mpv.play(self._currentStation)
-        self.playing = True
-
-    def stop(self):
-        self._mpv.stop()
-        self.playing = False
-
-    @staticmethod
-    def _getStations():
-        with open("stations.txt", "r") as file:
-            lines = file.readlines()
-            return [station.strip("\n") for station in lines if station != "\n" or station.startswith("#")]
-
-
+from utils.player import Player
 
 
 app = Flask(__name__)
-#player = mpv.MPV(ytdl=False)
 player = Player()
 
 @app.route('/')
@@ -71,13 +31,13 @@ def radioControl():
         
 @app.route('/api/status')
 def status():
-    if player.playing:
-        return "playing"
-    else:
-        return "paused"
+    # TODO: json response
+    return player.status(), 200
 
 @app.route('/api/station')
 def station():
+    # TODO: rework
     return player.getStation(), 200
 
-app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
---
+++ src/utils/player.py
@@ -0,0 +1,59 @@
+import json
+import random
+
+from dataclasses import dataclass
+
+import mpv
+
+
+@dataclass
+class Station:
+    name: str
+    url: str
+
+
+class Player:
+    def __init__(self):
+        self._mpv = mpv.MPV()
+        self._stations = []
+        self._stationLoader()
+        self._currentStation = random.choice(self._stations)
+        self._playing = False
+
+    def getStation(self):
+        return self._currentStation
+
+    def setStation(self, station):
+        self._currentStation = station
+
+    def changeStation(self):
+        new = random.choice(self._stations)
+        if new == self._currentStation:
+            self.changeStation()
+        else:
+            self.setStation(new)
+            self.play()
+
+    def play(self) -> None:
+        self._mpv.play(self._currentStation.url)
+        self._playing = True
+
+    def stop(self) -> None:
+        self._mpv.stop()
+        self._playing = False
+
+    def status(self) -> str:
+        if self._playing:
+            return 'playing'
+        else:
+            return 'paused'
+        
+    def _stationLoader(self):
+        with open("stations.json", "r") as f:
+            j = json.load(f)
+            for station in j:
+                self._stations.append(
+                    Station(name=station['name'],
+                            url=station['url']
+                    )
+                )
