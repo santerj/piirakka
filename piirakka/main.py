b9c04622ee6eb107a193f32945208d5b02e42069
Santeri Suomala <santeri.suomala@outlook.com>
2024-11-26 00:42:24 +0200

    boilerplate main file, tbd

--- piirakka/main.py
+++ piirakka/main.py
@@ -1,14 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import model
+
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.requests import Request
+import model.sidebar_item
+
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="piirakka/templates")
 app.mount("/static", StaticFiles(directory="piirakka/static"), name="static")
 
-@app.get("/api/hello")
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request}) 
-
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html",
+                {
+                    "request": request,
+                    "sidebar_items": model.sidebar_item.sidebar_items
+                }
+    ) 
