236ca0f43c37bdf32b423afc7d0e98d10609f2b1
santerj <45292222+santerj@users.noreply.github.com>
2026-08-24 08:38:54 +0300

    enum for event types

--- piirakka/model/event.py
+++ piirakka/model/event.py
@@ -1,50 +1,41 @@
 """Definitions of events sent to subscribers over websocket."""
 
+from enum import Enum
+from typing import Generic, TypeVar
+
 from pydantic import BaseModel
 
-from piirakka.model.station import StationPydantic
 
-# from piirakka.model.player_state import PlayerState
-# from piirakka.model.recent_track import RecentTrack
-
-# TODO: clean up of commented out code
+class EventType(str, Enum):
+    """Match the event name in client side websocket code"""
+    PLAYER_BAR_UPDATED = "player_bar_updated"
+    STATIONS_CHANGED = "stations_changed"
+    SIDEBAR_CHANGED = "sidebar_changed"
+    TRACK_HISTORY_CHANGED = "track_history_changed"
+    BLUETOOTH_LIST_CHANGED = "bluetooth_list_changed"
 
 
 class PlayerBarUpdateEvent(BaseModel):
     """Any value represented in the player bar has changed - render entire player bar and broadcast."""
-
     content: str  # html
-    # content: PlayerState
-    event_type: str = "player_bar_updated"
-
+    event_type: EventType.PLAYER_BAR_UPDATED
 
 class StationListChangeEvent(BaseModel):
     """Station list has changed - render entire station list and broadcast."""
-
-    # TODO: event type unused and not understood by frontend
-    # TODO: should be refacored to send rendered html (consumed by station settings page)
-    # content: str  # html
-    content: list[StationPydantic]
-    event_type: str = "stations_changed"
-
+    content: str  # html
+    event_type: str = EventType.STATIONS_CHANGED
 
 class SidebarChangeEvent(BaseModel):
     """Sidebar has changed - render entire sidebar and broadcast."""
-
-    # TODO: event type unused and not understood by frontend
     content: str  # html
-    event_type: str = "sidebar_changed"
-
+    event_type: str = EventType.SIDEBAR_CHANGED
 
 class TrackChangeEvent(BaseModel):
     """Currently playing track changed - render entire track history and broadcast."""
-
     content: str  # html
-    # content: RecentTrack
-    event_type: str = "track_history_changed"
+    event_type: str = EventType.TRACK_HISTORY_CHANGED
 
 class BluetoothListChangeEvent(BaseModel):
     """Any value in the bluetooth device list changed - render entire list and broadcast."""
-
     content: str  # html
-    event_type: str = "bluetooth_list_changed"
+    event_type: str = EventType.BLUETOOTH_LIST_CHANGED
